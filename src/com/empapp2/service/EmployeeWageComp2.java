d7baf9c5da24e680fde268e00be3f3adb18b7312
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-13 17:47:32 +0530

    Added Functionality to check whether employee is present or absent

--- src/com/empapp2/service/EmployeeWageComp2.java
+++ src/com/empapp2/service/EmployeeWageComp2.java
@@ -1,6 +1,15 @@
 package com.empapp2.service;
 import com.empapp2.model.Employee;
-public class EmployeeWageComp2
+public class EmployeeWageComp2 implements EmpWageComp
 {
+	Employee emp2 = new Employee();
+	public void empCheck()
+	{
+	int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+		if(empCheck == emp2.getFullTime())
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}	
 }
 
--- src/com/empapp2/test/EmployeeTest.java
+++ src/com/empapp2/test/EmployeeTest.java
@@ -1,9 +1,14 @@
 package com.empapp2.test;
 import com.empapp2.model.Employee;
+import com.empapp2.service.*;
 public class EmployeeTest
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
+		EmpWageComp obj1 = new EmployeeWageComp1();
+		EmpWageComp obj2 = new EmployeeWageComp2();	
+		obj1.empCheck();
+		obj2.empCheck();
 	}
 }

f7fc3d47a292df83a74ebeec7b7dafc512f65b56
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-15 22:05:50 +0530

    Refactored to have one EmpWageBuilder to manage for Wage for multiple Companies using array of Company objects

--- src/com/empapp2/service/IempWageComp.java
+++ src/com/empapp2/service/IempWageComp.java
@@ -1,9 +1,10 @@
 package com.empapp2.service;
-import com.empapp2.model.Employee;
+import com.empapp2.model.*;
 public interface IempWageComp
 {
+	public void addEmpWageCompany(String company, int wagePerHour,int fullTimeHours, int partTimeHours, int numOfWorkingDays, int maxWorkingHours);
 	public void computeEmpWage();
-	public int dailyFullWage();
-	public int dailyPartWage();
+	public int dailyFullWage(EmpWageCompany emp);
+	public int dailyPartWage(EmpWageCompany emp);
 }
 
